ca856217995b0e83b11371d97516e4be74bf81c5
Patrick-Yosafat <patyosardi@gmail.com>
2025-09-13 00:18:34 +0700

    First commit!

---
+++ cookbook/src/test/java/dev/arcaninar/cookbook/CookbookApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arcaninar.cookbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
